79886fb7ec34f3e0556eedee798212b5f3368612
Nick Griffin <nicholas.griffin@accenture.com>
2015-07-16 15:27:51 +0100

    DA-447: Adding Ant

---
+++ init.groovy.d/adop_ant.groovy
@@ -0,0 +1,49 @@
+import hudson.model.*;
+import jenkins.model.*;
+import hudson.tools.*;
+import hudson.tasks.Ant.AntInstaller;
+import hudson.tasks.Ant.AntInstallation;
+
+// Variables
+def ant_version = "1.9.4"
+
+// Constants
+def instance = Jenkins.getInstance()
+
+// env['USERNAME']
+
+Thread.start {
+    sleep 10000
+
+    // Ant
+    println "--> Configuring Ant"
+    def desc_AntTool = instance.getDescriptor("hudson.tasks.Ant")
+
+    def antInstaller = new AntInstaller(ant_version)
+    def installSourceProperty = new InstallSourceProperty([antInstaller])
+    def ant_inst = new AntInstallation(
+      "ADOP Ant", // Name
+      "", // Home
+      [installSourceProperty]
+    )
+
+    // Only add ADOP Ant if it does not exist - do not overwrite existing config
+    def ant_installations = desc_AntTool.getInstallations()
+    def ant_inst_exists = false
+    ant_installations.each {
+      installation = (AntInstallation) it
+        if ( ant_inst.getName() ==  installation.getName() ) {
+                ant_inst_exists = true
+                println("Found existing installation: " + installation.getName())
+        }
+    }
+    
+    if (!ant_inst_exists) {
+        ant_installations += ant_inst
+        desc_AntTool.setInstallations((AntInstallation[]) ant_installations)
+        desc_AntTool.save()
+    }
+
+    // Save the state
+    instance.save()
+}
