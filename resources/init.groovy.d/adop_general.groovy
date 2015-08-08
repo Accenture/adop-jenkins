38500f63700551db8637be083f07b2f733221f65
Nick Griffin <nicholas.griffin@accenture.com>
2015-08-08 16:29:13 +0100

    DA-447: Adding SSH key registration and Git identity

--- resources/init.groovy.d/adop_general.groovy
+++ resources/init.groovy.d/adop_general.groovy
@@ -1,5 +1,8 @@
 import hudson.model.*;
 import jenkins.model.*;
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey;
+import com.cloudbees.plugins.credentials.CredentialsScope;
 
 // Variables
 def env = System.getenv()
@@ -36,6 +39,40 @@ Thread.start {
 
     // Example
     //envVars.put("FOO", "bar")
+
+    // Jenkins SSH Credentials
+    println "--> Registering SSH Credentials"
+    def system_credentials_provider = SystemCredentialsProvider.getInstance()
+
+    def ssh_key_description = "ADOP Jenkins Master"
+
+    ssh_credentials_exist = false
+    system_credentials_provider.getCredentials().each {
+        credentials = (com.cloudbees.plugins.credentials.Credentials) it
+        if ( credentials.getDescription() == ssh_key_description) {
+            ssh_credentials_exist = true
+            println("Found existing credentials: " + ssh_key_description)
+        }
+    }
+
+    if(!ssh_credentials_exist) {
+        def ssh_key_scope = CredentialsScope.GLOBAL
+        def ssh_key_id = null
+        def ssh_key_username = "jenkins"
+        def ssh_key_private_key_source = new BasicSSHUserPrivateKey.UsersPrivateKeySource()
+        def ssh_key_passphrase = null
+
+        def ssh_key_domain = com.cloudbees.plugins.credentials.domains.Domain.global()
+        def ssh_key_creds = new BasicSSHUserPrivateKey(ssh_key_scope,ssh_key_id,ssh_key_username,ssh_key_private_key_source,ssh_key_passphrase,ssh_key_description)
+
+        system_credentials_provider.addCredentials(ssh_key_domain,ssh_key_creds)
+    }
+
+    // Git Identity
+    println "--> Configuring Git Identity"
+    def desc_git_scm = instance.getDescriptor("hudson.plugins.git.GitSCM")
+    desc_git_scm.setGlobalConfigName("ADOP Jenkins")
+    desc_git_scm.setGlobalConfigEmail("jenkins@adop")
 
     // Save the state
     instance.save()
