e35eba48eaeb2be358dfbf8370bd6252f7db5a24
Nick Griffin <nicholas.griffin@accenture.com>
2015-08-10 11:34:12 +0100

    DA-447: Only adding ADOP Gerrit server configuration to trigger if not already present

--- resources/init.groovy.d/adop_gerrit.groovy
+++ resources/init.groovy.d/adop_gerrit.groovy
@@ -33,20 +33,33 @@ Thread.start {
     def gerrit_trigger_plugin = PluginImpl.getInstance()
 
     def gerrit_server = new GerritServer("ADOP Gerrit")
-    def gerrit_server_config = new Config()
 
-    gerrit_server_config.setGerritHostName(gerrit_host_name)
-    gerrit_server_config.setGerritFrontEndURL(gerrit_front_end_url)
-    gerrit_server_config.setGerritSshPort(gerrit_ssh_port)
-    gerrit_server_config.setGerritUserName(gerrit_username)
-    gerrit_server_config.setGerritEMail(gerrit_email)
-    gerrit_server_config.setGerritAuthKeyFile(new File(gerrit_ssh_key_file))
-    gerrit_server_config.setGerritAuthKeyFilePassword(gerrit_ssh_key_password)
+    def gerrit_servers = gerrit_trigger_plugin.getServerNames()
+    def gerrit_server_exists = false
+    gerrit_servers.each {
+        server_name = (String) it
+        if ( server_name == gerrit_server.getName() ) {
+            gerrit_server_exists = true
+            println("Found existing installation: " + server_name)
+        }
+    }
 
-    gerrit_server.setConfig(gerrit_server_config)
-    gerrit_trigger_plugin.addServer(gerrit_server)
-    gerrit_server.start()
-    gerrit_server.startConnection()
+    if (!gerrit_server_exists) {
+        def gerrit_server_config = new Config()
+
+        gerrit_server_config.setGerritHostName(gerrit_host_name)
+        gerrit_server_config.setGerritFrontEndURL(gerrit_front_end_url)
+        gerrit_server_config.setGerritSshPort(gerrit_ssh_port)
+        gerrit_server_config.setGerritUserName(gerrit_username)
+        gerrit_server_config.setGerritEMail(gerrit_email)
+        gerrit_server_config.setGerritAuthKeyFile(new File(gerrit_ssh_key_file))
+        gerrit_server_config.setGerritAuthKeyFilePassword(gerrit_ssh_key_password)
+
+        gerrit_server.setConfig(gerrit_server_config)
+        gerrit_trigger_plugin.addServer(gerrit_server)
+        gerrit_server.start()
+        gerrit_server.startConnection()
+    }
 
     // Save the state
     instance.save()
