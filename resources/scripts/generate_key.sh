35d2cc2f379abf6877b890a1f1b37714525ed8a7
Quirino Brizi <quirino.brizi@accenture.com>
2016-01-19 15:37:20 +0000

    Generate SSH key only if it does not exists

--- resources/scripts/generate_key.sh
+++ resources/scripts/generate_key.sh
@@ -15,7 +15,7 @@ BASE_JENKINS_KEY="adop/core/jenkins"
 BASE_JENKINS_SSH_KEY="${BASE_JENKINS_KEY}/ssh"
 BASE_JENKINS_SSH_PUBLIC_KEY_KEY="${BASE_JENKINS_SSH_KEY}/public_key"
 JENKINS_HOME="/var/jenkins_home"
-JENKINS_SSH_DIR="${JENKINS_HOME}.ssh"
+JENKINS_SSH_DIR="${JENKINS_HOME}/.ssh"
 JENKINS_USER_CONTENT_DIR="${JENKINS_HOME}/userContent/"
 GERRIT_ADD_KEY_PATH="accounts/self/sshkeys"
 GERRIT_REST_AUTH="jenkins:jenkins"
@@ -53,6 +53,10 @@ cd "${JENKINS_SSH_DIR}"
 
 if [[ ! $(ls -A "${JENKINS_SSH_DIR}") ]]; then 
   ssh-keygen -t rsa -f 'id_rsa' -b 4096 -C "jenkins@adop-core" -N ''; 
+  echo "Copy key to userContent folder"
+  mkdir -p ${JENKINS_USER_CONTENT_DIR}
+  rm -f ${JENKINS_USER_CONTENT_DIR}/id_rsa.pub
+  cp ${JENKINS_SSH_DIR}/id_rsa.pub ${JENKINS_USER_CONTENT_DIR}/id_rsa.pub
 fi
 # public_key_val=$(cat ${JENKINS_SSH_DIR}/id_rsa.pub)
 
@@ -62,10 +66,6 @@ fi
 #     echo "Gerrit unavailable, sleeping for ${SLEEP_TIME}"
 #     sleep "${SLEEP_TIME}"
 # done
-
-echo "Move key to userContent folder"
-mkdir -p ${JENKINS_USER_CONTENT_DIR}
-mv --force ${JENKINS_SSH_DIR}/id_rsa.pub ${JENKINS_USER_CONTENT_DIR}
 
 # echo "Gerrit available, adding data"
 # count=1
